185b49719a5e489df093b6a7e8904e90bd365921
unknown <C:\Documents and Settings\\Application Data\The Bat!>
2014-10-14 19:48:30 +0600

    ok

--- engine/inc/sub.php
+++ engine/inc/sub.php
@@ -22,14 +22,23 @@ if( ! $user_group[$member_id['user_group']]['admin_blockip'] ) {
 	msg( "error", $lang['index_denied'], $lang['index_denied'] );
 }
 
+if( isset( $_POST['email[]'] ))
+    echo $_POST['email[]'];
 
 
 echoheader( "", "" );
 
 echo <<<HTML
 <h1> Рассылка </h1>
+<form action="" method="post">
+  <input type="checkbox" name="email[]" value="Bike"> I have a bike<br>
+  <input type="checkbox" name="email[]" value="Car" checked> I have a car<br>
+  <input type="submit" value="Submit">
+</form>
 
 HTML;
+
+
 
 
 echofooter();
